631d8debda1ddc7c64d1eb0186d5a22bbe05b531
Matheus Richard Chignolli <matheuschignolli@gmail.com>
2020-07-29 20:39:08 -0300

    chore: config sass

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,29 @@ module.exports = {
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: ['.ts', '.tsx', '.js', 'scss', 'sass'],
     alias: {
       '@': path.join(__dirname, 'src')
     }
+  },
+  module = {
+    rules: [{
+      test: /\.ts(x?)$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.s[ac]ss$/,
+      use: [{
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader',
+        options: {
+          modules: true
+        }
+      }, {
+        loader: 'sass-loader'
+      }]
+    }]
   },
   devServer: {
     contentBase: './public',
