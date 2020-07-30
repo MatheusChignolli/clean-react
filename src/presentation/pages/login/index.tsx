75467ece9797527cb612a0507bf0ce28f5795124
Matheus Richard Chignolli <matheuschignolli@gmail.com>
2020-07-30 11:48:10 -0300

    refactor: move logo to a separate file

---
+++ src/presentation/pages/login/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Styles from './styles.sass'
+import Spinner from '@/presentation/components/spinner'
+import Logo from '@/presentation/components/Logo'
+
+const Login: React.FC = () => {
+  return (
+    <div className={Styles.login}>
+      <header className={Styles.header}>
+        <Logo/>
+        <h1>4Dev - Enquetes para Programadores</h1>
+      </header>
+      <form className={Styles.form}>
+        <h2>Login</h2>
+        <div className={Styles.inputWrap}>
+          <input type="email" name="email" placeholder="Digite seu e-mail"/>
+          <span className={Styles.status}></span>
+        </div>
+        <div className={Styles.inputWrap}>
+          <input type="password" name="password" placeholder="Digiste sua senha"/>
+          <span className={Styles.status}></span>
+        </div>
+        <button className={Styles.submit} type="submit">Entrar</button>
+        <span className={Styles.link}>Criar conta</span>
+        <div className={Styles.errorWrap}>
+          <Spinner className={Styles.spinner}/>
+          <span className={Styles.error}>Erro</span>
+        </div>
+      </form>
+      <footer className={Styles.footer}></footer>
+    </div>
+  )
+}
+
+export default Login
